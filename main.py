c582cfeb416daec2fe1f79e6986eb4b3c6ce4e9d
BOOPESH-foxy <boopesh.mc@gmail.com>
2023-04-09 23:29:17 +0530

    user and admin

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from sqlalchemy import create_engine
 import mysql.connector
 import sqlalchemyDbconnect as db
 import pwinput
+import os
+import tableCreation
 
 class loginPage:
     def __init__(self) -> None:
@@ -9,15 +11,17 @@ class loginPage:
 
     
     def getData(self):
+        os.system('clear')
         print("|\t\t\t COVID VACCINATION BOOKING \t\t\t|")
-        name = input("NAME : \t")
-        password = input()
+        name = input("\nId : \t")
+        password = pwinput.pwinput(prompt="password : ",mask ="*")
+        tableCreation.dbProcesses.checkUserstatus(name,password)
     
     def pageSelection(self):
         pass
     
     
 if __name__ == "__main__":
-    obj = loginPage()
-    
+    mainObj = loginPage()
+    mainObj.getData()
     
--- tableCreation.py
+++ tableCreation.py
@@ -1,10 +1,12 @@
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine,Table,VARCHAR, Column, Integer, String , MetaData
 from sqlalchemy.orm import declarative_base, sessionmaker
 import sqlalchemyDbconnect
+from sqlalchemy.sql import schema
 
 engine = create_engine(sqlalchemyDbconnect.url)
 session = sessionmaker(bind=engine)
 Session = session()
+metadata = MetaData()
 
 Base = declarative_base()
 class MyTable(Base):
@@ -14,27 +16,32 @@ class MyTable(Base):
     isStaff = Column(Integer)
 
 Base.metadata.create_all(engine)
-
+my_table = Table('Authentication', metadata) #, autoload=True, autoload_with=engine)
+new_column = Column('password', VARCHAR(30))
+alterTable = schema.AddColumn(my_table,new_column)
+with engine.connect() as conn:
+    conn.execute(alterTable)
 users = Session.query(MyTable).all()
-for user in users:
-    print(user.Id, user.name,)
+Session.commit()
 
-class dbProcesses:
+
+# class dbProcesses:
     
-    def __init__(self,Id,name,isStaff) -> None:
-        self.id = Id
-        self.name = name
-        self.isStaff = isStaff
+#     def __init__(self) -> None:
+#         pass
     
-    def inserInto(self):
-        row = MyTable(Id = self.id,name=self.name, isStaff = self.isStaff)
-        Session.add(row)
-        self.commit()
+#     def inserInto(self,id,name,idStaff):
+#         row = MyTable(Id = self.id,name=self.name, isStaff = self.isStaff)
+#         Session.add(row)
+#         self.commit()
         
-    def checkUserstatus(self):
-        onlyStaff = Session.query(MyTable).filter(MyTable.isStaff == 1).all()
-        for staffs in active_employees:
-            print(employee.name)
-    
-    def commit():
-        Session.commit()
+#     def checkUserstatus(self,Id,password):
+#         inputId = Id
+#         password = password
+#         onlyStaff = Session.query(MyTable).filter(MyTable.isStaff == 1).all()
+#         for staffs in onlyStaff:
+#             print(onlyStaff.Id)
+        
+
+#     def commit():
+#         Session.commit()
