bb6b99c722fe0d7e3380d285fbe2f415bd9a4848
BOOPESH-foxy <boopesh.mc@gmail.com>
2023-04-13 16:27:46 +0530

    tabulation

--- adminPage.py
+++ adminPage.py
@@ -12,10 +12,15 @@ class AdminPage:
         self.hospitalId = 0
         self.applicants=[]
         self.applicant_selected=[]
+        
     def find_hospital(self):
         self.db.cursor.execute("SELECT * FROM hospitals WHERE hospitalAdmin = %s",(self.user.userName,))
         myresult = self.db.cursor.fetchall()
         self.hospitalId=myresult[0][0]
         self.hospitalName=myresult[0][1]
         
-    
+    def view_slots(self):
+        self.db.cursor.execute("SELECT * FROM %s_slots",(self.hospitalId,))
+        mvResult = self.db.cursor.fetchall()
+        mvResult.insert(0,["Slot id","Begining (HH:MM)","Ending (HH:MM)","Total Slots","Available Slots","Date"])
+        print(tabulate(mvResult,showindex='always',headers='firstrow'))
