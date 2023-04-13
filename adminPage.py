d99d10941eca1e482c9e6ec4bbd5ca174a90e689
BOOPESH-foxy <boopesh.mc@gmail.com>
2023-04-13 13:35:31 +0530

    find hospital function

---
+++ adminPage.py
@@ -0,0 +1,21 @@
+from os import system, name
+from tabulate import tabulate
+from startpage import user
+import datetime
+from datetime import date
+
+class AdminPage:
+    def __init__(self,db,user:user):
+        self.db = db
+        self.user = user
+        self.hospitalName = ""
+        self.hospitalId = 0
+        self.applicants=[]
+        self.applicant_selected=[]
+    def find_hospital(self):
+        self.db.cursor.execute("SELECT * FROM hospitals WHERE hospitalAdmin = %s",(self.user.userName,))
+        myresult = self.db.cursor.fetchall()
+        self.hospitalId=myresult[0][0]
+        self.hospitalName=myresult[0][1]
+        
+    
