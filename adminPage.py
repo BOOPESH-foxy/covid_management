de8b28d3161f6cf054a947b33cb3764e00638ecd
BOOPESH-foxy <boopesh.mc@gmail.com>
2023-04-18 20:24:30 +0530

    pageStartup

--- adminPage.py
+++ adminPage.py
@@ -25,5 +25,21 @@ class AdminPage:
         mvResult.insert(0,["Slot id","Begining (HH:MM)","Ending (HH:MM)","Total Slots","Available Slots","Date"])
         print(tabulate(mvResult,showindex='always',headers='firstrow'))
         
-    def pageStarup():
-        pass
+    def pageStarup(self):
+        self.find_hospital()
+        print("Hello admin, {} of hospital {} (id {})".format(self.user.userName,self.hospitalName,self.hospitalId))
+        admin_case = int(input("[1] Create/View Slots \n[2] Approve Slots \n[3] Record vaccination \nchoose [1-3]:"))
+        system('clear')
+        if(admin_case == 1):
+            self.view_slots()
+            create_bool = int(input("Do you want to add a new slot? [0-1]:"))
+            while(create_bool):
+                if(0):
+                    break
+                else:
+                    system("clear")
+                    self.create_slot()
+                    if(int(input("Do you want to continue adding? [0-1]:"))==0):
+                        break
+                    else:
+                        pass
